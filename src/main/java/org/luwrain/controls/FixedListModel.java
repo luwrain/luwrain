6ff75f2123d88b26f2dd823ca9c3a7b4369e2e90
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-12-09 01:41:54 +0700

    controls.FixedListModel now based on java.util.Vector

--- src/main/java/org/luwrain/controls/FixedListModel.java
+++ src/main/java/org/luwrain/controls/FixedListModel.java
@@ -1,82 +1,43 @@
-/*
-   Copyright 2012-2016 Michael Pozhidaev <michael.pozhidaev@gmail.com>
-
-   This file is part of the LUWRAIN.
-
-   LUWRAIN is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
-
-   LUWRAIN is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-*/
 
 package org.luwrain.controls;
 
 import java.util.*;
 
-import org.luwrain.core.NullCheck;
+import org.luwrain.core.*;
 
-public class FixedListModel implements ListArea.Model
+public class FixedListModel extends Vector implements ListArea.Model
 {
-    private Object[] items;
-
     public FixedListModel()
     {
-	items = new Object[0];
     }
 
     public FixedListModel(Object[] items)
     {
 	NullCheck.notNullItems(items, "items");
-	this.items = items;
+	setItems(items);
     }
 
     public void setItems(Object[] items)
     {
-			  NullCheck.notNullItems(items, "items");
-			  this.items = items;
-    }
-
-    public void clear()
-    {
-	items = new Object[0];
+	NullCheck.notNullItems(items, "items");
+	setSize(items.length);
+	for(int i = 0;i < items.length;++i)
+	    set(i, items[i]);
     }
 
     public Object[] getItems()
     {
-	return items;
-    }
-
-    public void add(Object o)
-    {
-	NullCheck.notNull(o, "o");
-	items = Arrays.copyOf(items, items.length + 1);
-	items[items.length - 1] = o;
-    }
-
-    public boolean delete(int index)
-    {
-	if (index < 0 || index >= items.length)
-	    return false;
-	if (index + 1 < items.length)
-	    for(int i = index;i < items.length - 1;++i)
-		items[index] = items[index + 1];
-	items = Arrays.copyOf(items, items.length - 1);
-	return true;
+	return toArray(new Object[size()]);
     }
 
     @Override public int getItemCount()
     {
-	return items.length;
+	return size();
     }
 
     @Override public Object getItem(int index)
     {
-	return index < items.length?items[index]:null;
+	return get(index);
     }
 
     @Override public void refresh()
