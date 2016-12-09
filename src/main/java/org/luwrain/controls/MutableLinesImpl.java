9adfa457d5cca11979f49f26d6ab2c5ff12cf4a6
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-12-09 23:16:38 +0700

    Cleaning controls.MutableLinesImpl

--- src/main/java/org/luwrain/controls/MutableLinesImpl.java
+++ src/main/java/org/luwrain/controls/MutableLinesImpl.java
@@ -1,25 +1,9 @@
-/*
-   Copyright 2012-2016 Michael Pozhidaev <michael.pozhidaev@gmail.com>
-
-   This file is part of the LUWRAIN.
-
-   LUWRAIN is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
-   License as published by the Free Software Foundation; either
-   version 3 of the License, or (at your option) any later version.
-
-   LUWRAIN is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   General Public License for more details.
-*/
 
 package org.luwrain.controls;
 
 import java.util.Vector;
-import org.luwrain.core.NullCheck;
-import org.luwrain.core.MutableLines;
-//import org.luwrain.util.Strings;
+
+import org.luwrain.core.*;
 
 public class MutableLinesImpl implements MutableLines
 {
@@ -32,17 +16,20 @@ public class MutableLinesImpl implements MutableLines
     public MutableLinesImpl(String[] lines)
     {
 	NullCheck.notNullItems(lines, "lines");
-	final String[] l = lines;
-	for(String ll: l)
-	    this.lines.add(ll);
+	this.lines.setSize(lines.length);
+	for(int i = 0;i < lines.length;++i)
+	    this.lines.set(i, lines[i]);
     }
 
     public MutableLinesImpl(String lines)
     {
 	NullCheck.notNull(lines, "lines");
+	if (lines.isEmpty())
+	    return;
 	final String[] l = lines.split("\n", -1);
-	for(String ll: l)
-	    this.lines.add(ll);
+	this.lines.setSize(l.length);
+	for(int i = 0;i < l.length;++i)
+	    this.lines.set(i, l[i]);
     }
 
     @Override public void beginLinesTrans()
@@ -68,10 +55,9 @@ public class MutableLinesImpl implements MutableLines
     public void setLines(String[] lines)
     {
 	NullCheck.notNullItems(lines, "lines");
-	final String[] l = lines;
-	this.lines.clear();
-	for(String ll: l)
-	    this.lines.add(ll);
+	this.lines.setSize(lines.length);
+	for(int i = 0;i < lines.length;++i)
+	    this.lines.set(i, lines[i]);
     }
 
     public String[] getLines()
@@ -81,27 +67,30 @@ public class MutableLinesImpl implements MutableLines
 
     @Override public void setLine(int index, String line)
     {
+	NullCheck.notNull(line, "line");
 	while(lines.size() <= index)
 	    lines.add("");
-	lines.set(index, line != null?line:"");
+	lines.set(index, line);
     }
 
     @Override public void addLine(String line)
     {
-	lines.add(line != null?line:"");
+	NullCheck.notNull(line, "line");
+	lines.add(line);
     }
 
     //index is the position of newly inserted line
     @Override public void insertLine(int index, String line)
     {
+	NullCheck.notNull(line, "line");
 	if (index < lines.size())
 	{
-	    lines.insertElementAt(line != null?line:"", index);
+	    lines.insertElementAt(line, index);
 	    return;
 	}
 	while(lines.size() < index)
 	    lines.add("");
-	lines.add(line != null?line:"");
+	lines.add(line);
     }
 
     @Override public void removeLine(int index)
@@ -118,7 +107,7 @@ public class MutableLinesImpl implements MutableLines
 
     public String getWholeText()
     {
-	if (lines.size() < 1)
+	if (lines.size() == 1)
 	    return "";
 	if (lines.size() == 1)
 	    return lines.get(0);
@@ -126,6 +115,6 @@ public class MutableLinesImpl implements MutableLines
 	res.append(lines.get(0));
 	for(int i = 1;i < lines.size();++i)
 	    res.append("\n" + lines.get(i));
-	return res.toString();
+	return new String(res);
     }
 }
