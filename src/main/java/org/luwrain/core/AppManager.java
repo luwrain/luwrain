cadfd9b581aab3d21122d3c0107b7e3e5025e3a2
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-12-23 04:19:22 +0700

    Fix to the bug with trying to check active app without active apps

--- src/main/java/org/luwrain/core/AppManager.java
+++ src/main/java/org/luwrain/core/AppManager.java
@@ -61,6 +61,8 @@ class AppManager
 	NullCheck.notNull(app, "app");
 	if (app == defaultApp && activeAppIndex < 0)
 	    return true;
+	if (activeAppIndex < 0)
+	    return false;
 	if (apps.get(activeAppIndex).app == app)
 	    return true;
 	return false;
