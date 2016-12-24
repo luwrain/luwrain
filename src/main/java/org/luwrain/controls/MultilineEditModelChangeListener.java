55ea169e52698434182e4134dba9a8d53cc0ef43
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2016-12-24 08:21:00 +0700

    Minor Javadoc fix

--- src/main/java/org/luwrain/controls/MultilineEditModelChangeListener.java
+++ src/main/java/org/luwrain/controls/MultilineEditModelChangeListener.java
@@ -5,7 +5,7 @@ import org.luwrain.core.*;
 
 /**
  * Implements a listener of all changes in 
- * {@link MultilineEditModel}. This class contains the abstract method 
+ * {@link MultilineEdit.Model}. This class contains the abstract method 
  * {@code onMultilineEditChange} called each time when any changes occurred in
  * the state of the model.  This allows users to implement any necessary
  * actions, which should have effect if and only if something was changed
