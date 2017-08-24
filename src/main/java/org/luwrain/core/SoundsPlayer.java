2c587635bf234e8acf274b3b4dac60f980e043d9
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2017-08-24 22:21:13 +0700

    Threads safety fixes to core.SoundsPlayer

--- src/main/java/org/luwrain/core/SoundsPlayer.java
+++ src/main/java/org/luwrain/core/SoundsPlayer.java
@@ -27,7 +27,7 @@ class SoundsPlayer
     {
     private static final int BUF_SIZE = 512;
 
-	private String fileName;
+	private final String fileName;
 	private boolean interruptPlayback = false;
 	boolean finished = false;
 private SourceDataLine audioLine = null;
@@ -40,52 +40,58 @@ private SourceDataLine audioLine = null;
 
 	@Override public void run()
 	{
+	    AudioInputStream audioInputStream = null;
 	    try {
-	    try {
-	    final File soundFile = new File(fileName);
-	    if (!soundFile.exists())
-		return;
-	    final AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(soundFile);
-	    final AudioFormat format = audioInputStream.getFormat();
-	    final DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-	    synchronized (this) {
-		audioLine = (SourceDataLine) AudioSystem.getLine(info);
-		audioLine.open(format);
-		audioLine.start();
-	    }
-	    int bytesRead = 0;
-	    byte[] buf = new byte[BUF_SIZE];
-		while (bytesRead != -1 && !interruptPlayback)
-		{
-		    bytesRead = audioInputStream.read(buf, 0, buf.length);
-		    //		    System.out.println("bytesRead=" + bytesRead);
-		    if (bytesRead >= 0)
-			synchronized (this) {
-			audioLine.write(buf, 0, bytesRead);
-			}
+		try {
+		    final File soundFile = new File(fileName);
+		    if (!soundFile.exists())
+			return;
+		    audioInputStream = AudioSystem.getAudioInputStream(soundFile);
+		    final AudioFormat format = audioInputStream.getFormat();
+		    final DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+		    audioLine = (SourceDataLine) AudioSystem.getLine(info);
+		    audioLine.open(format);
+		    audioLine.start();
+		    int bytesRead = 0;
+		    final byte[] buf = new byte[BUF_SIZE];
+		    while (bytesRead != -1 && !interruptPlayback)
+		    {
+			bytesRead = audioInputStream.read(buf, 0, buf.length);
+			if (bytesRead >= 0)
+			    audioLine.write(buf, 0, bytesRead);
+		    }
+		    audioLine.drain();
 		}
-		audioLine.drain();
-	}
-	    finally
-	    {
-		if (audioLine != null)
-		audioLine.close();
-		finished = true;
-	    }
-	    }
+		finally {
+		    synchronized(this)
+		    {
+			if (audioLine != null)
+			{
+			    audioLine.close();
+			    audioLine = null;
+			}
+			if (audioInputStream != null)
+			{
+			    audioInputStream.close();
+			    audioInputStream = null;
+			}
+		    }
+		    finished = true;
+		}
+	    } //try
 	    catch(UnsupportedAudioFileException | IOException | LineUnavailableException e)
 	    {
-		e.printStackTrace();
+		Log.error(Environment.LOG_COMPONENT, "unable to play audio file" + fileName + ":" + e.getClass().getName() + ":" + e.getMessage());
 	    }
-		}
+	}
 
 	void stopPlaying()
 	{
 	    interruptPlayback = true;
-	    if (audioLine != null)
+	    synchronized(this)
 	    {
-		//		audioLine.flush();
-	    audioLine.stop();
+		if (audioLine != null)
+		    audioLine.stop();
 	    }
 	}
     }
