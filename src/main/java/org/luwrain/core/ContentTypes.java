4c232ae7630e8db24fe1ff695d7a7353d2a7ae1f
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-01-07 15:53:50 +0700

    core.WavePlayers gets an implementation of MediaResourcePlayer

---
+++ src/main/java/org/luwrain/core/ContentTypes.java
@@ -0,0 +1,11 @@
+
+package org.luwrain.core;
+
+interface ContentTypes
+{
+    static final String[] SOUND_WAVE = new String[]{
+	"audio/vnd.wave",
+    };
+
+    static final String SOUND_WAVE_DEFAULT = SOUND_WAVE[0];
+}
--- src/main/java/org/luwrain/core/WavePlayers.java
+++ src/main/java/org/luwrain/core/WavePlayers.java
@@ -88,4 +88,126 @@ class WavePlayers
 	    }
 	}
     }
+
+    static private class PlayerInstance implements MediaResourcePlayer.Instance
+    {
+	private static final int NOTIFY_MSEC_COUNT=500;
+	private static final int BUF_SIZE = 512;
+
+	private boolean interruptPlayback = false;
+	private SourceDataLine audioLine = null;
+	AudioFormat format=null;
+	private final MediaResourcePlayer.Listener listener;
+	private FutureTask<Boolean> futureTask = null;
+    private final ExecutorService executor = Executors.newSingleThreadExecutor();
+
+	PlayerInstance(MediaResourcePlayer.Listener listener)
+	{
+	    NullCheck.notNull(listener, "listener");
+	    this.listener = listener;
+	}
+
+	@Override public MediaResourcePlayer.Result play(URL url, long playFromMsec, Set<MediaResourcePlayer.Flags> flags)
+	{
+	    NullCheck.notNull(url, "url");
+	    NullCheck.notNull(flags, "flags");
+	    interruptPlayback = false;
+	    NullCheck.notNull(url, "url");
+	    NullCheck.notNull(flags, "flags");
+	    AudioInputStream audioInputStream;
+	    try {
+		audioInputStream=AudioSystem.getAudioInputStream(url.openStream());
+	    } 
+	    catch(Exception e)
+	    {
+		e.printStackTrace();
+		listener.onPlayerFinish(PlayerInstance.this);
+		return new MediaResourcePlayer.Result();
+	    }
+	    format=audioInputStream.getFormat();
+	    futureTask = new FutureTask<>(()->{
+		    try
+		    {
+			final DataLine.Info info=new DataLine.Info(SourceDataLine.class,format);
+			synchronized(this)
+			{
+			    audioLine=(SourceDataLine)AudioSystem.getLine(info);
+			    // FloatControl volume=(FloatControl)line.getControl(FloatControl.Type.MASTER_GAIN); 
+			    audioLine.open(format);
+			    audioLine.start();
+			}
+			long totalBytes=0;
+			// skip if task need it
+			if(playFromMsec > 0)
+			{
+			    // bytes count from msec pos, 8000 is a 8 bits in byte and 1000 ms in second
+			    long skipBytes=mSecToBytesSamples(playFromMsec);
+			    audioInputStream.skip(skipBytes);
+			    totalBytes+=skipBytes;
+			}
+			long lastNotifiedMsec=totalBytes;
+			long notifyBytesCount=mSecToBytesSamples(NOTIFY_MSEC_COUNT);
+			int bytesRead=0;
+			byte[] buf=new byte[BUF_SIZE];
+			while(bytesRead!=-1&&!interruptPlayback)
+			{
+			    bytesRead=audioInputStream.read(buf,0,buf.length);
+			    // System.out.println("bytesRead=" + bytesRead);
+			    if(bytesRead>=0) synchronized(this)
+					     {
+						 audioLine.write(buf,0,bytesRead);
+						 totalBytes+=bytesRead;
+					     }
+			    if (totalBytes > lastNotifiedMsec + notifyBytesCount)
+			    {
+				lastNotifiedMsec = totalBytes;
+				listener.onPlayerTime(PlayerInstance.this, (long)bytesSamplesTomSec(totalBytes));
+				//Log.debug("player","SoundPlayer: step"+(long)bytesSamplesTomSec(totalBytes));
+			    }
+			}
+			audioLine.drain();
+		    } catch(Exception e)
+		    {
+			e.printStackTrace();
+			listener.onPlayerFinish(PlayerInstance.this);
+			return false;
+		    } finally
+		    {
+			if(audioLine!=null) audioLine.close();
+		    }
+		    //Log.debug("player","SoundPlayer: finish");
+		    listener.onPlayerFinish(PlayerInstance.this);
+		    return true;
+		});
+	    executor.execute(futureTask);
+	    return new MediaResourcePlayer.Result();
+	}
+
+	private long mSecToBytesSamples(float msec)
+	{
+	    return (long)(format.getSampleRate()*format.getSampleSizeInBits()*msec/8000);
+	}
+	private float bytesSamplesTomSec(long samples)
+	{
+	    return (8000f*samples/format.getSampleRate()*format.getSampleSizeInBits());
+	}
+	@Override public void stop()
+	{
+	    interruptPlayback=true;
+	}
+    }
+
+    class Player implements MediaResourcePlayer
+    {
+	@Override public Instance newMediaResourcePlayer(Listener listener)
+	{
+	    NullCheck.notNull(listener, "listener");
+	    return new PlayerInstance(listener);
+	}
+
+	@Override public String getSupportedMimeType()
+	{
+	    return ContentTypes.SOUND_WAVE_DEFAULT;
+	}
+    }
 }
