694f8812551858216caa51e9b6b8957963136e08
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-05-07 05:14:15 +0700

    Cleaning CenteredArea

--- src/main/java/org/luwrain/controls/CenteredArea.java
+++ src/main/java/org/luwrain/controls/CenteredArea.java
@@ -1,3 +1,18 @@
+/*
+   Copyright 2012-2018 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
 
 package org.luwrain.controls;
 
@@ -131,7 +146,7 @@ public class CenteredArea implements org.luwrain.core.Area
 	context.onAreaNewHotPoint(this);
     }
 
-        public void setLocalHotPointY(int value)
+    public void setLocalHotPointY(int value)
     {
 	if (value < 0)
 	    throw new IllegalArgumentException("value (" + value + ") may not be negative");
@@ -145,7 +160,7 @@ public class CenteredArea implements org.luwrain.core.Area
 	for(String s: lines)
 	    if (res < s.length())
 		res = s.length();
-		return res;
+	return res;
     }
 
     public int getLeft()
