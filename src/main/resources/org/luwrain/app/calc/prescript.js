c5a52ee2d262f3eaf6beb734d3fb334f0a0d2997
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-07-29 23:03:01 +0700

    calc: Adding prescript.js

---
+++ src/main/resources/org/luwrain/app/calc/prescript.js
@@ -0,0 +1,24 @@
+function abs(x){return java.lang.Math.abs(x);}
+function acos(x){return java.lang.Math.acos(x);}
+function asin(x){return java.lang.Math.asin(x);}
+function atan(x){return java.lang.Math.atan(x);}
+function cbrt(x){return java.lang.Math.cbrt(x);}
+function cos(x){return java.lang.Math.cos(x);}
+function cosh(x){return java.lang.Math.cosh(x);}
+function exp(x){return java.lang.Math.exp(x);}
+function floor(x){return java.lang.Math.floor(x);}
+function log(x){return java.lang.Math.log(x);}
+function log10(x){return java.lang.Math.log10(x);}
+function random(x){return java.lang.Math.random(x);}
+function round(x){return java.lang.Math.round(x);}
+function sin(x){return java.lang.Math.sin(x);}
+function sinh(x){return java.lang.Math.sinh(x);}
+function sqrt(x){return java.lang.Math.sqrt(x);}
+function tan(x){return java.lang.Math.tan(x);}
+function toDegrees(x){return java.lang.Math.toDegrees(x);}
+function toRadians(x){return java.lang.Math.toRadians(x);}
+
+function max(x, y) {return java.lang.Math.max(x, y);}
+function min(x, y) {return java.lang.Math.min(x, y);}
+function pow(x, y) {return java.lang.Math.pow(x, y);}
+
