cdf069b07fccf3ad93cf65eb2082560c9c22e76e
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2018-08-02 22:41:05 +0700

    Adding the DownloadManager interface

---
+++ src/main/java/org/luwrain/io/download/DownloadManager.java
@@ -0,0 +1,34 @@
+/*
+   Copyright 2012-2018 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+//LWR_API 1.0
+
+package org.luwrain.io.download;
+
+import java.util.*;
+
+public interface DownloadManager
+{
+    public enum Flags{
+    };
+
+    String startDownload(String url, String destFilePath, int attemptCount, Set<Flags> flags);
+    void closeDownload(String downloadId);
+    boolean isDownloadFinished(String downloadId);
+    String getUrl(String downloadId);
+    String getDestFilePath(String downloadId);
+    LocalDownload startLocalDownload(String url, String destFilePath, int attemptCount, Set<Flags> flags);
+}
