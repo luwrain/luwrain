bfe93f911726c190cdc6a844cb01adae46911e58
Michael Pozhidaev <michael.pozhidaev@gmail.com>
2019-03-11 22:39:16 +0700

    Adding PropertiesHookObject and SimpleShortcutCommand

---
+++ src/main/java/org/luwrain/core/SimpleShortcutCommand.java
@@ -0,0 +1,50 @@
+/*
+   Copyright 2012-2019 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+//LWR_API 1.0
+
+package org.luwrain.core;
+
+public class SimpleShortcutCommand implements Command
+{
+    protected final String cmdName;
+    protected final String shortcutName;
+
+    public SimpleShortcutCommand(String cmdName, String shortcutName)
+    {
+	NullCheck.notEmpty(cmdName, "cmdName");
+	NullCheck.notNull(shortcutName, "shortcutName");
+	this.cmdName = cmdName;
+	this.shortcutName = shortcutName;
+    }
+
+        public SimpleShortcutCommand(String name)
+    {
+	this(name, name);
+    }
+
+
+    @Override public String getName()
+    {
+	return cmdName;
+    }
+
+    @Override public void onCommand(Luwrain luwrain)
+    {
+	NullCheck.notNull(luwrain, "luwrain");
+	luwrain.launchApp(shortcutName);
+    }
+}
---
+++ src/main/java/org/luwrain/script/PropertiesHookObject.java
@@ -0,0 +1,55 @@
+
+package org.luwrain.script;
+
+import java.util.*;
+import jdk.nashorn.api.scripting.*;
+
+import org.luwrain.core.*;
+
+public class PropertiesHookObject extends EmptyHookObject
+{
+    private final Properties props;
+    private final String propName;
+
+    public PropertiesHookObject(Properties props, String propName)
+    {
+	NullCheck.notNull(props, "props");
+	NullCheck.notNull(propName, "propName");
+	this.props = props;
+	this.propName = propName;
+    }
+
+    @Override public Object getMember(String name)
+    {
+	NullCheck.notNull(name, "name");
+	if (name.isEmpty())
+	    return super.getMember(name);
+	if (propName.isEmpty())
+	    return new PropertiesHookObject(props, name);
+	return new PropertiesHookObject(props, propName + "." + name);
+    }
+
+    @Override public void setMember(String name, Object value)
+    {
+	NullCheck.notNull(name, "name");
+	if (name.isEmpty() || value == null)
+	    return;
+	final String text = value.toString();
+	if (text == null)
+	    return;
+	if (!propName.isEmpty())
+	props.setProperty(propName + "." + name, text); else
+	    	props.setProperty(name, text);
+    }
+
+    @Override public Object getDefaultValue(Class hint)
+    {
+	final String res = props.getProperty(propName);
+	return res != null?res:"";
+    }
+
+    @Override public String toString()
+    {
+	return getDefaultValue(String.class).toString();
+    }
+}
