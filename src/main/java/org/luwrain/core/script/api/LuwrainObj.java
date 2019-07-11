b4fcc004ca64d583382da27ceecc2a4ac732792a
Michael Pozhidaev <msp@luwrain.org>
2019-07-12 04:56:53 +0700

    Fixed the bug with multiple hooks

--- src/main/java/org/luwrain/core/script/api/LuwrainObj.java
+++ src/main/java/org/luwrain/core/script/api/LuwrainObj.java
@@ -177,10 +177,10 @@ public final class LuwrainObj extends AbstractJSObject
 	if (!(func instanceof jdk.nashorn.api.scripting.ScriptObjectMirror))
 	    return false;
 	final  jdk.nashorn.api.scripting.ScriptObjectMirror funcObj = (jdk.nashorn.api.scripting.ScriptObjectMirror)func;
-	if (!hooks.containsKey(name.toString()))
 	    if (!funcObj.isFunction())
 		return false;
-	    hooks.put(name.toString(), new LinkedList());
+	    	if (!hooks.containsKey(name.toString()))
+	    	    hooks.put(name.toString(), new LinkedList());
 	hooks.get(name.toString()).add(new org.luwrain.core.script.api.Hook(funcObj));
 	return true;
     }
