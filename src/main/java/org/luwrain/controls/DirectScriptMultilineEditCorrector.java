be4546cb0a5f2ca63c81929501ee849b39d502cf
Michael Pozhidaev <msp@luwrain.org>
2019-08-12 08:23:02 +0700

    Javadoc for DirectScriptMultilinEditCorrector

--- src/main/java/org/luwrain/controls/DirectScriptMultilineEditCorrector.java
+++ src/main/java/org/luwrain/controls/DirectScriptMultilineEditCorrector.java
@@ -14,6 +14,8 @@
    General Public License for more details.
 */
 
+//LWR_API 2.0
+
 package org.luwrain.controls;
 
 import java.util.*;
@@ -27,6 +29,41 @@ import org.luwrain.util.*;
 
 import org.luwrain.controls.MultilineEdit2.ModificationResult;
 
+/**
+ * A translator of text correcting operations to hooks actions. This
+ * class can wrap any instance of {@link MultilineEditCorrector} with
+ * calls of the hooks prior and after of the corresponding action of the
+ * underlying corrector.
+ * <p>
+ * The call of the hook prior to the operation may cancel the operation,
+ * if it returns {@code false}. If it returns any other value , including
+ * {@code null} and {@code undefined}, the processing will be
+ * continued. The hooks called after the operation may return any value,
+ * it is never taken into account in any way.
+ * <p>
+ * All hooks handlers must expect the single argument with members,
+ * which set depends on the purpose of the particular operation. In the
+ * meantime, the argument always has the {@code lines} and {@code
+ * hotPoint} members, since they are actual for any operation.
+ * <p>
+ * Here is the list of the hooks created by this class where {@code base}
+ * is a string value provided with the constructor:
+ * <p>
+ * <ul>
+ *   <li>{@code base.delete.char.pre}</li>
+ *   <li>{@code base.delete.char.post}</li>
+ *   <li>{@code base.delete.region.pre}</li>
+ *   <li>{@code base.delete.region.post}</li>
+ *   <li>{@code base.insert.region.pre}</li>
+ *   <li>{@code base.insert.region.post}</li>
+ *   <li>{@code base.insert.chars.pre}</li>
+ *   <li>{@code base.insert.chars.post}</li>
+ *   <li>{@code base.merge.lines.pre}</li>
+ *   <li>{@code base.merge.lines.post}</li>
+ *   <li>{@code base.split.lines.pre}</li>
+ *   <li>{@code base.split.lines.post}</li>
+ * </ul>
+ */
 public class DirectScriptMultilineEditCorrector implements MultilineEditCorrector2
 {
     static protected final String LOG_COMPONENT = "controls";
@@ -74,32 +111,31 @@ public class DirectScriptMultilineEditCorrector implements MultilineEditCorrecto
     {
 	if (!runPreHook(hookNameBase + ".delete.char.pre"))
 	    return new ModificationResult(false);
-final ModificationResult res = base.deleteChar(pos, lineIndex);
-if (!res.isPerformed())
-    return res;
-runPostHook(hookNameBase + ".delete.char.post");
-return res;
+	final ModificationResult res = base.deleteChar(pos, lineIndex);
+	if (res.isPerformed())
+	runPostHook(hookNameBase + ".delete.char.post");
+	return res;
     }
 
     @Override public ModificationResult deleteRegion(int fromX, int fromY, int toX, int toY)
     {
 	if (!runPreHook(hookNameBase + ".delete.region.pre"))
 	    return new ModificationResult(false);
-final ModificationResult res = base.deleteRegion(fromX, fromY, toX, toY);
-if (!res.isPerformed())
-    return res;
-runPostHook(hookNameBase + ".delete.region.post");
-return res;
+	final ModificationResult res = base.deleteRegion(fromX, fromY, toX, toY);
+	if (res.isPerformed())
+	runPostHook(hookNameBase + ".delete.region.post");
+	return res;
     }
 
     @Override public ModificationResult insertRegion(int x, int y, String[] lines)
     {
-		NullCheck.notNullItems(lines, "lines");
-		if (!runPreHook(hookNameBase + ".insert.region.pre"))
-		    return new ModificationResult(false);
-final ModificationResult res = base.insertRegion(x, y, lines);
-runPostHook(hookNameBase + ".insert.region.post");
-		    return res;
+	NullCheck.notNullItems(lines, "lines");
+	if (!runPreHook(hookNameBase + ".insert.region.pre"))
+	    return new ModificationResult(false);
+	final ModificationResult res = base.insertRegion(x, y, lines);
+	if (res.isPerformed())
+	    runPostHook(hookNameBase + ".insert.region.post");
+	return res;
     }
 
     @Override public ModificationResult putChars(int pos, int lineIndex, String str)
@@ -109,29 +145,27 @@ runPostHook(hookNameBase + ".insert.region.post");
 	    return new ModificationResult(false);
 	final ModificationResult res = base.putChars(pos, lineIndex, str);
 	if (res.isPerformed())
-	runPostHook(hookNameBase + ".insert.chars.post");
+	    runPostHook(hookNameBase + ".insert.chars.post");
 	return res;
     }
 
     @Override public ModificationResult mergeLines(int firstLineIndex)
     {
-				if (!runPreHook(hookNameBase + ".merge.lines.pre"))
-		    return new ModificationResult(false);
-final ModificationResult res = base.mergeLines(firstLineIndex);
-if (!res.isPerformed())
-    return res;
-runPostHook(hookNameBase + ".merge.lines.post");
-				    return res;
+	if (!runPreHook(hookNameBase + ".merge.lines.pre"))
+	    return new ModificationResult(false);
+	final ModificationResult res = base.mergeLines(firstLineIndex);
+	if (res.isPerformed())
+	    runPostHook(hookNameBase + ".merge.lines.post");
+	return res;
     }
 
     @Override public ModificationResult splitLine(int pos, int lineIndex)
     {
-			if (!runPreHook(hookNameBase + ".split.lines.pre"))
-		    return new ModificationResult(false);
+	if (!runPreHook(hookNameBase + ".split.lines.pre"))
+	    return new ModificationResult(false);
 	final ModificationResult res = base.splitLine(pos, lineIndex);
-	if (!res.isPerformed())
-	return res;
-	runPostHook(hookNameBase + ".split.post");
+	if (res.isPerformed())
+	    runPostHook(hookNameBase + ".split.post");
 	return res;
     }
 
@@ -195,7 +229,7 @@ runPostHook(hookNameBase + ".merge.lines.post");
 	return mayContinue.get();
     }
 
-        protected boolean runPostHook(String hookName)
+    protected boolean runPostHook(String hookName)
     {
 	NullCheck.notEmpty(hookName, "hookName");
 	final AtomicBoolean success = new AtomicBoolean(true);
@@ -220,14 +254,14 @@ runPostHook(hookNameBase + ".merge.lines.post");
 		final AtomicReference ex = new AtomicReference();
 		context.runHooks(hookName, (hook)->{
 			try {
-hook.run(new Object[]{arg});
+			    hook.run(new Object[]{arg});
 			}
 			catch(RuntimeException e)
 			{
 			    ex.set(e);
 			    return Luwrain.HookResult.BREAK;
 			}
-			    return Luwrain.HookResult.CONTINUE;
+			return Luwrain.HookResult.CONTINUE;
 		    });
 		if (ex.get() != null)
 		{
