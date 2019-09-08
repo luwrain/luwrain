86c450f6dc01c6a43c91adcaff81a1f691f6da69
Michael Pozhidaev <msp@luwrain.org>
2019-09-08 12:18:30 +0700

    io.CommanderUtilsVfs goes to io.git

--- src/main/java/org/luwrain/io/CommanderUtilsFile.java
+++ src/main/java/org/luwrain/io/CommanderUtilsFile.java
@@ -1,5 +1,5 @@
 /*
-   Copyright 2012-2017 Michael Pozhidaev <michael.pozhidaev@gmail.com>
+   Copyright 2012-2019 Michael Pozhidaev <msp@luwrain.org>
 
    This file is part of LUWRAIN.
 
@@ -13,6 +13,8 @@
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
    General Public License for more details.
 */
+
+//LWR_API 1.0
 
 package org.luwrain.io;
 
