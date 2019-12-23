7187ea094e3963173ad60dfb31358f58ca29fb01
Michael Pozhidaev <msp@luwrain.org>
2019-12-23 08:57:58 +0700

    New code for controls.UndoLines

--- src/main/java/org/luwrain/controls/UndoLines.java
+++ src/main/java/org/luwrain/controls/UndoLines.java
@@ -16,66 +16,143 @@
 
 package org.luwrain.controls;
 
+import java.util.*;
+
 import org.luwrain.core.*;
 
 public class UndoLines implements MutableLines 
 {
+    protected final MutableLines lines;
+    protected final List<Command> commands = new LinkedList();
+
+    public UndoLines(MutableLines lines)
+    {
+	NullCheck.notNull(lines, "lines");
+	this.lines = lines;
+    }
 
     @Override public int getLineCount()
     {
-	return 0;
+	return lines.getLineCount();
     }
 
     @Override public String getLine(int index)
     {
-	return null;
+	return lines.getLine(index);
     }
-    
+
     @Override public void beginLinesTrans()
     {
     }
-    
+
     @Override public void endLinesTrans()
     {
     }
-    
+
     @Override public String[] getLines()
     {
-	return null;
+	return lines.getLines();
     }
-    
+
     @Override public void setLines(String[] lines)
     {
     }
-    
+
     @Override public void addLine(String line)
     {
+	NullCheck.notNull(line, "line");
+	final Command cmd = new AddLine(lines, line);
+	cmd.redo(lines);
+	saveCommand(cmd);
     }
-    
+
     @Override public void insertLine(int index, String line)
     {
+	//FIXME:
     }
-    
+
     @Override public void removeLine(int index)
     {
+	//New command will take care about index bounds
+	final Command cmd = new RemoveLine(lines, index);
+	cmd.redo(lines);
+	saveCommand(cmd);
     }
-    
+
     @Override public void setLine(int index, String line)
     {
     }
-    
+
     @Override public void clear()
     {
     }
 
-        @Override public LineMarks getLineMarks(int index)
+    @Override public LineMarks getLineMarks(int index)
     {
 	return null;
     }
-    
+
     @Override public void setLineMarks(int index, LineMarks lineMarks)
     {
     }
 
-    
+    protected void saveCommand(Command command)
+    {
+	NullCheck.notNull(command, "command");
+	commands.add(command);
+    }
+
+    static protected abstract class Command
+    {
+	public abstract void redo(MutableLines lines);
+	public abstract void undo(MutableLines lines);
+    }
+
+    static protected final class AddLine extends Command
+    {
+	private final int addedLineIndex;
+	private final String line;
+	public AddLine(MutableLines lines, String line)
+	{
+	    NullCheck.notNull(lines, "lines");
+	    NullCheck.notNull(line, "line");
+	    this.addedLineIndex = lines.getLineCount();
+	    this.line = line;
+	}
+	@Override public void redo(MutableLines lines)
+	{
+	    NullCheck.notNull(lines, "lines");
+	    lines.addLine(line);
+	}
+	@Override public void undo(MutableLines lines)
+	{
+	    NullCheck.notNull(lines, "lines");
+	    lines.removeLine(addedLineIndex);
+	}
+    }
+
+        static protected final class RemoveLine extends Command
+    {
+	private final int removingLineIndex;
+	private final String line;
+	public RemoveLine(MutableLines lines, int index)
+	{
+	    NullCheck.notNull(lines, "lines");
+	    if (index < 0 || index >= lines.getLineCount())
+		throw new IllegalArgumentException("index (" + String.valueOf(index) + ") must be non-negative and less than " + String.valueOf(lines.getLineCount()));
+	    this.removingLineIndex = lines.getLineCount();
+	    this.line = lines.getLine(index);
+	}
+	@Override public void redo(MutableLines lines)
+	{
+	    NullCheck.notNull(lines, "lines");
+	    lines.removeLine(removingLineIndex);
+	}
+	@Override public void undo(MutableLines lines)
+	{
+	    NullCheck.notNull(lines, "lines");
+	    lines.insertLine(removingLineIndex, line);
+	}
+    }
+
 }
