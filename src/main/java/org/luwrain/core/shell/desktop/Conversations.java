6101431e914536fb8ddc942f0653e8c5038a8a1c
Michael Pozhidaev <msp@luwrain.org>
2020-08-17 12:53:54 +0700

    Adding desktop.Strings and desktop.Conversations

---
+++ src/main/java/org/luwrain/core/shell/desktop/Conversations.java
@@ -0,0 +1,51 @@
+/*
+   Copyright 2012-2020 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.core.shell.desktop;
+
+import java.util.*;
+import java.io.*;
+
+import org.luwrain.core.*;
+import org.luwrain.core.events.*;
+import org.luwrain.core.queries.*;
+import org.luwrain.popups.*;
+import org.luwrain.util.*;
+
+final class Conversations
+{
+    private final Luwrain luwrain;
+    private final Strings strings;
+
+    Conversations(Luwrain luwrain, Strings strings)
+    {
+	NullCheck.notNull(luwrain, "luwrain");
+	NullCheck.notNull(strings, "strings");
+	this.luwrain = luwrain;
+	this.strings = strings;
+    }
+
+boolean deleteItem(String name)
+    {
+	NullCheck.notNull(name, "name");
+	return Popups.confirmDefaultNo(luwrain, strings.deleteItemsPopupName(), strings.deleteSingleItemPopup(name));
+    }
+
+    public boolean deleteItems(int count)
+    {
+	return Popups.confirmDefaultNo(luwrain, strings.deleteItemsPopupName(), strings.deleteItemsPopup(count));
+    }
+}
