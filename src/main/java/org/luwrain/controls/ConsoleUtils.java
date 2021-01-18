2b000a02b17e9757ed88aaaa1821210f6aa894d8
Michael Pozhidaev <msp@luwrain.org>
2021-01-19 06:26:29 +0700

    Adding controls.ConsoleUtils

---
+++ src/main/java/org/luwrain/controls/ConsoleUtils.java
@@ -0,0 +1,52 @@
+/*
+   Copyright 2012-2021 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+//LWR_API 1.0
+
+package org.luwrain.controls;
+
+import org.luwrain.core.*;
+
+public final class ConsoleUtils
+{
+        static public class ArrayModel implements ConsoleArea.Model
+    {
+	public interface Source
+	{
+	    Object[] getItems();
+	}
+	protected final Source source;
+	public ArrayModel(Source source)
+	{
+	    NullCheck.notNull(source, "source");
+	    this.source = source;
+	}
+	@Override public int getItemCount()
+	{
+	    final Object[] o = source.getItems();
+	    return o != null?o.length:0;
+	}
+	@Override public Object getItem(int index)
+	{
+	    final Object[] o = source.getItems();
+	    if (o == null)
+		return "#No items#";
+	    if (index < 0 || index >= o.length)
+		return "#Illegal index: " + String.valueOf(index) + "#";
+	    return o[index];
+	}
+    }
+}
