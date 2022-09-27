77398073251b2b18bbbc3d0aefd335d74b078794
Michael Pozhidaev <msp@luwrain.org>
2022-09-28 03:17:55 +0700

    Adding controls.TreeListArea

---
+++ src/main/java/org/luwrain/controls/TreeListArea.java
@@ -0,0 +1,222 @@
+/*
+   Copyright 2012-2022 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.controls;
+
+import java.util.*;
+
+import org.luwrain.core.*;
+import org.luwrain.core.events.*;
+
+import static org.luwrain.core.DefaultEventResponse.*;
+
+public class TreeListArea<E> extends ListArea<E> implements ListArea.ClickHandler<E>
+{
+    public interface Collector<E>
+    {
+	void collect(List<E> items);
+    }
+
+public interface Model<E>
+{
+    E getRoot();
+    boolean getItems(E root, Collector<E> collector);
+    boolean isLeaf(E item);
+}
+
+    public interface LeafClickHandler<E>
+    {
+	boolean onLeafClick(TreeListArea<E> area, E item);
+    }
+
+    static public final class Params<E>
+    {
+	public ControlContext context;
+	public String name;
+	public TreeListArea.Model<E> model = null;
+	public LeafClickHandler leafClickHandler = null;
+    }
+
+    protected final TreeListArea.Model<E> model;
+    protected List<Frame> history = new ArrayList<>();
+    protected final List<E> content;
+    protected LeafClickHandler leafClickHandler = null;
+
+    public TreeListArea(TreeListArea.Params<E> params)
+    {
+	super(createListParams(params));
+	NullCheck.notNull(params.model, "params.model");
+	this.model = params.model;
+	this.leafClickHandler = params.leafClickHandler;
+	this.content = ((ModelImpl)getListModel()).getSource();
+	setListClickHandler(this);
+    }
+
+    public void setLeafClickHandler(LeafClickHandler leafClickHandler)
+    {
+	NullCheck.notNull(leafClickHandler, "leafClickHandler");
+	this.leafClickHandler = leafClickHandler;
+    }
+
+    public boolean requery()
+    {
+	if (history.isEmpty())
+	    history.add(new Frame<E>(model.getRoot()));
+	return fill(getLastFrame().parent);
+    }
+
+    protected boolean fill(E obj, E select)
+    {
+	return model.getItems(obj, (items)->{
+		if (items == null)
+		    throw new NullPointerException("items can't be null");
+		this.content.clear();
+		this.content.addAll(items);
+		reset(false);
+		refresh();
+		if (select != null)
+		    this.select(select, false);
+	    });
+    }
+
+        protected boolean fill(E obj)
+    {
+	return fill(obj, null);
+    }
+
+    protected boolean fill(Frame<E> frame)
+    {
+	return fill(frame.parent, frame.selected);
+    }
+
+    @Override public boolean onListClick(ListArea area, int index, E item)
+    {
+	if (model.isLeaf(item))
+	{
+	getLastFrame().selected = selected();
+	history.add(new Frame<E>(item));
+	return fill(item);
+	}
+	if (leafClickHandler != null)
+	    return leafClickHandler.onLeafClick(this, item);
+	return false;
+    }
+
+    @Override public boolean onInputEvent(InputEvent event)
+    {
+	NullCheck.notNull(event, "event");
+	if (event.isSpecial() && !event.isModified())
+	    switch(event.getSpecial())
+	    {
+	    case BACKSPACE:
+		return onBackspace(event);
+	    }
+	return super.onInputEvent(event);
+    }
+
+    protected boolean onBackspace(InputEvent event)
+    {
+	if (history.size() <= 1)
+	    return false;
+	history.remove(history.size() - 1);
+	return fill(getLastFrame());
+    }
+
+    protected Frame<E> getLastFrame()
+    {
+	if (history.isEmpty())
+	    throw new IllegalStateException("No frames in the history");
+	return history.get(history.size() - 1);
+    }
+
+    static protected <E> ListArea.Params<E> createListParams(TreeListArea.Params params)
+    {
+	NullCheck.notNull(params, "params");
+	NullCheck.notNull(params.model, "params.model");
+final ListArea.Params<E> res = new ListArea.Params<>();
+res.context = params.context;
+res.name = params.name;
+res.model = new ModelImpl<E>(new ArrayList<E>());
+res.appearance = new Appearance<E>(res.context){
+	@Override public boolean isLeaf(E e)
+	{
+	    return params.model.isLeaf(e);
+	}
+    };
+return res;
+    }
+
+    static protected class ModelImpl<E> extends ListUtils.ListModel<E>
+    {
+	public ModelImpl(List<E> source) { super(source); }
+	public List<E> getSource() { return source; }
+    }
+
+    static protected final class Frame<E>
+    {
+	public final E parent;
+	E selected = null;
+	public Frame(E parent)
+	{
+	    NullCheck.notNull(parent, "parent");
+	    this.parent = parent;
+	}
+    }
+
+static abstract    protected class Appearance<E> extends ListUtils.DefaultAppearance<E>
+    {
+	Appearance(ControlContext context) { super(context); }
+	abstract public boolean isLeaf(E e);
+	@Override public void announceItem(E e, Set<Flags> flags)
+	{
+	    if (isLeaf(e))
+		context.setEventResponse(listItem(Sounds.ATTENTION, e.toString(), Suggestions.CLICKABLE_LIST_ITEM)); else
+		context.setEventResponse(listItem(e.toString(), Suggestions.LIST_ITEM));
+	}
+    }
+
+    /*
+    static public class Adapter implements ListArea.Model<Object>
+    {
+	protected final TreeArea.Model treeModel;
+	protected Object opened;
+	protected final ArrayList<Object> items = new ArrayList<>();
+	public Adapter(TreeArea.Model treeModel)
+	{
+	    NullCheck.notNull(treeModel, "treeModel");
+	    this.treeModel = treeModel;
+	    this.opened = treeModel.getRoot();
+	    refresh();
+	}
+	@Override public int getItemCount() { return items.size(); }
+	@Override public Object getItem(int index) { return items.get(index); }
+	@Override public void refresh()
+	{
+	    treeModel.beginChildEnumeration(opened);
+	    try {
+		final int count = treeModel.getChildCount(opened);
+		this.items.clear();
+		this.items.ensureCapacity(count);
+		for(int i = 0;i < count;i++)
+		    this.items.add(treeModel.getChild(opened, i));
+	    }
+	    finally {
+		treeModel.endChildEnumeration(opened);
+	    }
+	}
+    }
+    */
+}
