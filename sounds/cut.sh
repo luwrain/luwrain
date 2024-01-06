0833e39b38c1a7e90cf8f9fb47294dca2f5f23ab
Michael Pozhidaev <msp@luwrain.org>
2024-01-06 15:28:12 +0700

    New sounds scripts

--- sounds/cut.sh
+++ sounds/cut.sh
@@ -3,9 +3,9 @@
 # The LUWRAIN Project, GPL v.3
 
 sox -D -n -r 48000 -c 2 -b 32 01-1.wav \
-    synth 5 sin G4 sin G5 synth 5 sin fmod G5 fade l 0 1 1 gain -1 trim 0 0.1
+    synth 5 sin B4 sin B5 synth 5 sin fmod B5 fade l 0 1 1 gain -1 trim 0 0.1
 sox -D -n -r 48000 -c 2 -b 32 01-2.wav \
-    synth 5 sin G2 synth 5 sin fmod G1 fade l 0 1 1 gain -10 trim 0 0.1
+    synth 5 sin B2 synth 5 sin fmod B1 fade l 0 1 1 gain -10 trim 0 0.1
 sox -D 01-1.wav 01-2.wav -m 01.wav
 
 sox -D -n -r 48000 -c 2 -b 32 02-1.wav \
@@ -17,5 +17,5 @@ sox -D 02-1.wav 02-2.wav -m 02.wav
 sox -D 01.wav 02.wav 03.wav bass 5
 sox -D 03.wav 04.wav pad 0 1
 sox 04.wav 05.wav REVERB 50 100 100 35 35
-sox --norm=-0.5 05.wav -c 2 -b 16 -r 44100 copy.wav
+sox --norm=-0.5 05.wav -c 2 -b 16 -r 44100 cut.wav
 rm -f 0?.wav 0?-?.wav
--- sounds/inaccessible.sh
+++ sounds/inaccessible.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 sox -D -n -b 32 -c 2 -r 44100 01.wav \
-    synth 1 sin %-1 sin %2 sin %5 sin fmod %-1  gain -3 \
-    fade l 0.1 0.2 0.1 pad 0 1
+    synth 1 pl %-1 sin %11 sin %14 sin %17 sin fmod %-1  gain -3 \
+    fade t 0 0.2 0.1 treble -10 pad 0 2
 sox -D 01.wav 02.wav \
-    reverb 50 10 50 30 100 1
+    reverb 85 100 35 50 50
 sox -D --norm=-0.5 02.wav -b 16 -c 2 -r 44100 inaccessible.wav
 rm -f 0?.wav
 
