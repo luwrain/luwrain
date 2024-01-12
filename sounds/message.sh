31bf02ba29a0c06dee3fe0f1bf43216fdd88bb33
Michael Pozhidaev <msp@luwrain.org>
2024-01-12 11:20:08 +0700

    Fixing message.sh

--- sounds/message.sh
+++ sounds/message.sh
@@ -3,23 +3,19 @@
 # The LUWRAIN Project, GPL v.3
 
 sox -D -n -r 48000 -c 2 -b 32 01-1.wav \
-    synth 5 sin %7 sin %-1 sin %2 sin fmod %7 \
+    synth 5 sin %4 sin %8 sin %16 sin %20 sin fmod %4 \
     fade l 0.05 0.125 0.05 gain -1
 sox -D -n -r 48000 -c 2 -b 32 01-2.wav \
-    synth 5 sin %7 sin %19 sin fmod %7 \
+    synth 5 sin %4 sin %16 sin fmod %4 \
     fade l 0.03 0.2 0.05 gain -1
 sox -D -n -r 48000 -c 2 -b 32 01-3.wav \
-    synth 5 sin %-5 sin fmod %7 \
-    fade l 0.05 0.2 0.05 gain -20
-sox -D -n -r 48000 -c 2 -b 32 01-4.wav \
-    synth 5 pl %-5 pl %7 sin fmod %7 \
-    fade t 0 0.7 0.5 gain -20 pad 0.035
-sox -D 01-1.wav 01-2.wav 01-3.wav 01-4.wav -m 01.wav pad 0.03 1
+    synth 5 pl %-8 pl %4 sin fmod %4 \
+    fade t 0 0.7 0.5 pad 0.035 gain -16
+sox -D 01-1.wav 01-2.wav 01-3.wav -m 01.wav pad 0.07 1
 
 sox -D -n -r 48000 -c 2 -b 32 02-1.wav \
-    synth 5 sin %2 sin %14 pl %14 sin fmod %-10 \
-    fade t 0.2 0.5 0.5 gain -20
-
+    synth 5 sin %-1 sin %11 pl %11 sin fmod %-13 \
+    fade t 0.2 0.5 0.5 gain -15
 
 sox 01.wav 02-1.wav -m 03.wav
 sox 03.wav 04.wav REVERB 75 100 50 100 100 5
