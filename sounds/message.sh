f345aa5906d577a615d2cbe9d00f63183d26c9fa
Michael Pozhidaev <msp@luwrain.org>
2024-01-12 08:52:53 +0700

    Updating message.sh

--- sounds/message.sh
+++ sounds/message.sh
@@ -3,7 +3,7 @@
 # The LUWRAIN Project, GPL v.3
 
 sox -D -n -r 48000 -c 2 -b 32 01-1.wav \
-    synth 5 sin %7 sin %11 sin %14 sin fmod %7 \
+    synth 5 sin %7 sin %-1 sin %2 sin fmod %7 \
     fade l 0.05 0.125 0.05 gain -1
 sox -D -n -r 48000 -c 2 -b 32 01-2.wav \
     synth 5 sin %7 sin %19 sin fmod %7 \
@@ -13,12 +13,12 @@ sox -D -n -r 48000 -c 2 -b 32 01-3.wav \
     fade l 0.05 0.2 0.05 gain -20
 sox -D -n -r 48000 -c 2 -b 32 01-4.wav \
     synth 5 pl %-5 pl %7 sin fmod %7 \
-    fade t 0 0.7 0.5 gain -17 pad 0.035
-sox -D 01-1.wav 01-2.wav 01-3.wav 01-4.wav -m 01.wav pad 0.03 2
+    fade t 0 0.7 0.5 gain -20 pad 0.035
+sox -D 01-1.wav 01-2.wav 01-3.wav 01-4.wav -m 01.wav pad 0.03 1
 
 sox -D -n -r 48000 -c 2 -b 32 02-1.wav \
     synth 5 sin %2 sin %14 pl %14 sin fmod %-10 \
-    fade t 0.2 0.5 0.5 gain -25
+    fade t 0.2 0.5 0.5 gain -20
 
 
 sox 01.wav 02-1.wav -m 03.wav
