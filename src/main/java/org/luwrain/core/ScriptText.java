ec578c5216434655090a621693920bedc69bc196
Michael Pozhidaev <msp@luwrain.org>
2024-03-02 16:45:56 +0700

    Adding core.ScriptSource

---
+++ src/main/java/org/luwrain/core/ScriptText.java
@@ -0,0 +1,36 @@
+/*
+   Copyright 2012-2024 Michael Pozhidaev <msp@luwrain.org>
+
+   This file is part of LUWRAIN.
+
+   LUWRAIN is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+
+   LUWRAIN is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+*/
+
+package org.luwrain.core;
+
+import static org.luwrain.core.NullCheck.*;
+
+public final class ScriptText extends ScriptSource
+{
+    private final String text;
+
+
+    public ScriptText(String text)
+    {
+	notNull(text, "text");
+	this.text = text;
+    }
+
+    public String getText()
+    {
+	return this.text;
+    }
+}
